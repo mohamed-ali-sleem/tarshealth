f186a69d069ecb5ff0de08ec3d2c18f36b989638
mosleem <mohamed.ali.fcih1992@gmail.com>
2022-02-06 11:09:35 +0200

    add changes

--- src/app/views/pages/catalog/details/details.component.ts
+++ src/app/views/pages/catalog/details/details.component.ts
@@ -21,22 +21,44 @@ Funnel3d(Highcharts);
 export class DetailsComponent implements OnInit {
   Highcharts: typeof Highcharts = Highcharts;
   categories = [
-    { id: 0, name: "procedures", isActive: true },
-    { id: 1, name: "conditions", isActive: false },
-    { id: 2, name: "medications", isActive: false },
+    { id: 0, name: "gender", isActive: true },
+    { id: 1, name: "age", isActive: false },
+    { id: 2, name: "procedures", isActive: false },
+    { id: 3, name: "conditions", isActive: false },
+    { id: 4, name: "medications", isActive: false },
   ];
+
+  dataList = []
+
+  genderList = [
+    { _id: 'M', description: 'Male' },
+    { _id: 'F', description: 'Female' }
+  ]
+
+  ageList = [
+    { _id: 1, fromage: 0, toage: 18 },
+    { _id: 2, fromage: 18, toage: 30 },
+    { _id: 3, fromage: 30, toage: 45 },
+    { _id: 4, fromage: 45, toage: 60 }
+  ]
+
   isLoading = false;
   selectedTab;
+
   filters = {
+    gender: '',
+    fromage: null,
+    toage: null,
     procedure: null,
     condition: null,
     medication: null
   }
 
+  gender: any;
+  age: any;
   procedure: any;
   condition: any;
   medication: any;
-
 
   patientList = []
 
@@ -92,8 +114,8 @@ export class DetailsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.changeCategory({ id: 0, name: "procedures", isActive: true })
-    this.getPatients()
+    this.changeCategory({ id: 0, name: "gender", isActive: true })
+    this.getPatients({}, 'add')
   }
 
 
@@ -104,6 +126,15 @@ export class DetailsComponent implements OnInit {
       element.isActive = false
     });
     this.categories[tab.id].isActive = true;
+    if (tab.name == 'gender') {
+      this.subCategotyList = [...this.genderList];
+      return
+    }
+
+    if (tab.name == 'age') {
+      this.subCategotyList = [...this.ageList];
+      return
+    }
     this._catalogService.getFilters(tab.name).subscribe(res => {
       console.log(res);
       this.subCategotyList = [...res.data];
@@ -115,21 +146,29 @@ export class DetailsComponent implements OnInit {
   }
 
 
-  getPatients(filters = {}) {
+  getPatients(filters = {}, type) {
     this.isLoading = true
     this._catalogService.getPatientList(filters).subscribe(res => {
       console.log(res);
       this.isLoading = false
-      this.patientList = [...res.data];
+      if (res.data) {
+        this.patientList = [...res.data];
+        type === 'add' ? this.dataList.unshift(res.data.length) : this.dataList.shift();
+
+      } else {
+        this.patientList = [];
+        type === 'add' ? this.dataList.unshift(0) : this.dataList.shift();
+      }
       const self = this;
       self.updateFromInput = true;
       self.chartOptions.series = [
         {
           type: "funnel3d",
           name: "Patients",
-          data: [ res.data.length],
+          data: [...this.dataList],
         },
       ];
+
     }, err => {
       console.log(err);
       this.isLoading = false
@@ -140,27 +179,40 @@ export class DetailsComponent implements OnInit {
 
   chageSubFilters(sub: any) {
     switch (this.selectedTab.name) {
+      case 'gender':
+        // code block 
+        this.gender = sub
+        this.filters.gender = this.gender._id
+        break;
+      case 'age':
+        // code block 
+        this.age = sub
+        this.filters.fromage = this.age.fromage;
+        this.filters.toage = this.age.toage
+        break;
       case 'procedures':
-        // code block
+        // code block 
         this.procedure = sub
         this.filters.procedure = this.procedure._id
         break;
       case 'conditions':
-        // code block
+        // code block 
         this.condition = sub
         this.filters.condition = this.condition._id
         break;
       default:
-        // code block
+        // code block 
         this.medication = sub
         this.filters.medication = this.medication._id
     }
-    this.getPatients(this.filters)
+    this.getPatients(this.filters, 'add')
   }
+
+
 
   clearFilter(val) {
     this.filters[val] = null;
     this[val] = null;
-    this.getPatients(this.filters)
+    this.getPatients(this.filters, 'delete')
   }
 }
